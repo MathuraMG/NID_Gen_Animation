08cc950ddb5f942f26e57ff3ad84c2e05c836455
Mathura MG <mmg9291@gmail.com>
2026-02-19 09:57:00 +0530

    add day3 - second half codes

--- Day_4/GenArt_Images/sketch.js
+++ Day_4/GenArt_Images/sketch.js
@@ -1,5 +1,7 @@
 
 let images = [];
+let degJump = 5;
+let imageChoices = [];
 
 function preload() {
   for(let i =0;i<6;i++) {
@@ -10,14 +12,31 @@ function preload() {
 function setup() {
   createCanvas(innerWidth, innerHeight);
   
-  frameRate(1);
+  frameRate(60);
+  angleMode(DEGREES);
+  
+  for(let i =0;i<100;i++) {
+    imageChoices[i] = floor(random(0,6));
+  }
 }
 
 function draw(){
   background(255);
-  for(let i =0;i<width;i+=200) {
-    for(let j =0;j<height;j+=200) {
-      image(images[floor(random(0,6))],i,j);
+  push();
+  translate(width/2, height/2);
+  for(let r = 0,i=0;r< height,i<100 ;r+=20,i++){
+    let imageChoice = imageChoices[i];
+    // let randomRotate = random(0,90);
+    for(let i = 0;i<360;i+=degJump) {
+      push();
+      translate(r*cos(i+mouseX), r*sin(i+mouseX))
+      rotate(i+90);
+      scale(0.2)
+      image(images[imageChoice],0,0);
+      pop();
     }
   }
+  
+  
+  pop();
 }
